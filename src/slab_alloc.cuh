b4711d1cf11d805d7eb47530f6a5a93ddd2c2591
Nicolas-Iskos <nki@andrew.cmu.edu>
2021-01-08 15:00:17 -0500

    bug fix for revised bit offsets

--- src/slab_alloc.cuh
+++ src/slab_alloc.cuh
@@ -110,7 +110,8 @@ class SlabAllocLightContext {
 
   __device__ __host__ __forceinline__ uint32_t
   getMemBlockIndex(SlabAllocAddressT address) const {
-    return ((address >> MEM_BLOCK_BIT_OFFSET_ALLOC_) & 0x1FFFF);
+    //return ((address >> MEM_BLOCK_BIT_OFFSET_ALLOC_) & 0x1FFFF);
+    return ((address >> MEM_BLOCK_BIT_OFFSET_ALLOC_) & 0x08FFF);
   }
 
   __device__ __host__ __forceinline__ SlabAllocAddressT
